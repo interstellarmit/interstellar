b537902b41aba33672d1c279aa10c269ccea0bdb
Akshaj Kadaveru <akshajk@mit.edu>
2020-08-02 11:07:57 -0400

    loungelistiteme

--- client/src/components/modules/LoungeListItem.js
+++ client/src/components/modules/LoungeListItem.js
@@ -4,25 +4,16 @@ import ProfilePic from "./ProfilePic";
 export default function LoungeListItem(props) {
   return (
     <List.Item onClick={props.redirect}>
-<<<<<<< HEAD
-      <List.Item.Meta 
+      <List.Item.Meta
         avatar={
           <Avatar.Group maxCount={6}>
-              {props.users.map((user)=>{return <ProfilePic user={user} />})}
-                </Avatar.Group>
+            {props.users.map((user) => {
+              return <ProfilePic user={user} />;
+            })}
+          </Avatar.Group>
         }
         title={props.name}
       />
-              
-              
-=======
-      <Avatar.Group maxCount={6}>
-        {props.users.map((user) => {
-          return <ProfilePic user={user} />;
-        })}
-      </Avatar.Group>
-      {props.name}
->>>>>>> ca76cca54ff7f57d66fab8c28b0ab52a08966d4a
     </List.Item>
   );
 }
