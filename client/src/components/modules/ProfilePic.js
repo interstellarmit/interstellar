e5275f05466c1deced3de122187552b8a3143301
Daniel Sun <dansun@mit.edu>
2020-08-07 19:21:56 -0700

    zoom link

--- client/src/components/modules/ProfilePic.js
+++ client/src/components/modules/ProfilePic.js
@@ -2,7 +2,8 @@ import React, { Component, useState } from 'react';
 import { Avatar } from 'antd';
 
 export default function ProfilePic(props) {
-  let initials = props.user.name.split(" ").map((name) => {return name[0]}).join("")
+  let initials = props.user.name.split(" ").map((name) => { return name[0] })
+  initials = initials[0] + initials[initials.length - 1]
   return (
     <Avatar>{initials}</Avatar>
   );
