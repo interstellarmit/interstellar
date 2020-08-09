a8a67e23051b73127dac640d09197d6eac5420e7
Daniel Sun <dansun@mit.edu>
2020-08-08 18:28:38 -0700

    fireroad

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -56,13 +56,13 @@ class App extends Component {
     } else if (window.location.href.indexOf("?code") > 0) {
       let code = window.location.href.substring(window.location.href.indexOf("?code"));
       self.state.code = code;
-      getJSON("https://fireroad-dev.mit.edu/fetch_token/" + code, (err, data) => {
+      getJSON("https://fireroad.mit.edu/fetch_token/" + code, (err, data) => {
         if (err !== null) {
           // alert("Something went wrong: " + err);
         } else {
           var req = new XMLHttpRequest();
           req.responseType = "json";
-          req.open("GET", "https://fireroad-dev.mit.edu/user_info/", true);
+          req.open("GET", "https://fireroad.mit.edu/user_info/", true);
           req.setRequestHeader("Authorization", "Bearer " + data.access_info.access_token);
           req.onload = function () {
             var jsonResponse = req.response;
@@ -104,7 +104,7 @@ class App extends Component {
 
     window.location.href =
       // "https://fireroad-dev.mit.edu/login?redirect=http%3A%2F%2Flocalhost%3A5000";
-      "https://fireroad-dev.mit.edu/login?redirect=https%3A%2F%2Finterstellar-beta.herokuapp.com";
+      "https://fireroad.mit.edu/login?redirect=https%3A%2F%2Finterstellar-beta.herokuapp.com";
   };
 
   signUpLogin = (data) => {
@@ -304,8 +304,8 @@ class App extends Component {
             },
           })
         ) : (
-          <></>
-        )}
+            <></>
+          )}
         <Layout style={{ minHeight: "100vh" }}>
           <SideBar
             pageIds={this.state.pageIds}
