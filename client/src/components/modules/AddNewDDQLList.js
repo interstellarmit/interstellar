eeaed6e0f4f7e0595da3176385d6857dcfe99a2d
Akshaj Kadaveru <akshajk@mit.edu>
2020-08-29 12:12:14 -0400

    ddql popup emptty

--- client/src/components/modules/AddNewDDQLList.js
+++ client/src/components/modules/AddNewDDQLList.js
@@ -1,5 +1,17 @@
 import React, { Component, useState, useEffect, useRef } from "react";
-import { List, Modal, Row, Col, Button, Form, Input, DatePicker, Checkbox } from "antd";
+import {
+  List,
+  Modal,
+  Row,
+  Col,
+  Button,
+  Form,
+  Input,
+  DatePicker,
+  Checkbox,
+  ConfigProvider,
+  Empty,
+} from "antd";
 import DueDate from "./DueDate";
 import moment from "moment";
 import QuickLink from "./QuickLink";
@@ -11,13 +23,25 @@ export default function AddNewDDQLList(props) {
   });
   return (
     <div style={{ height: "250px", overflow: "auto" }}>
-      <List
-        dataSource={props.dataSource}
-        renderItem={(item) => {
-          if (item.marker) return <div ref={scrollToRef} />;
-          return props.renderItem(item);
+      <ConfigProvider
+        renderEmpty={() => {
+          return <Empty description="Empty public feed" />;
         }}
-      />
+      >
+        <List
+          dataSource={
+            props.dataSource.filter((data) => {
+              return data.marker ? false : true;
+            }).length > 0
+              ? props.dataSource
+              : []
+          }
+          renderItem={(item) => {
+            if (item.marker) return <div ref={scrollToRef} />;
+            return props.renderItem(item);
+          }}
+        />
+      </ConfigProvider>
     </div>
   );
 }
